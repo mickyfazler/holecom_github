54a07e287a138f51bab8c5aea73fee790b339f2d
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2022-02-03 20:05:02 +0600

    Update settings.py

--- mysitebb/settings.py
+++ mysitebb/settings.py
@@ -15,7 +15,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['.holecom.com','holecombb.herokuapp.com','127.0.0.1:8000']
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://holecombb.herokuapp.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://holecomb.herokuapp.com','https://*.127.0.0.1']
 
 # Application definition
 
