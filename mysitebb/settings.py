97c69c22cc4ac0c40e9123308fc93cb5e4b8c7df
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2022-02-05 08:30:59 +0600

    changed allowed host for security

--- mysitebb/settings.py
+++ mysitebb/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = 'django-insecure-feas2ka+1q5(!+!rc%d%n16(un=7qk4w*cnxycp=-s4fq=r&#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['.holecom.com','holecombb.herokuapp.com','127.0.0.1:8000']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.holecom.com','holecombb.herokuapp.com','127.0.0.1:8000']
+# ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://holecomb.herokuapp.com','https://*.127.0.0.1']
 
 # Application definition
