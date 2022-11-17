66a7e76cd2a90cac7471f71accb7b1fb76750bc3
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2022-11-17 16:16:24 +0600

    dj_database_url given again

--- mysitebb/settings.py
+++ mysitebb/settings.py
@@ -99,10 +99,10 @@ DATABASES = {
     }
 }
 
-# import  dj_database_url     # here it's dj_datatabase_url but in requirements.txt dj-datatabase-url remember NOTE:
+import  dj_database_url     # here it's dj_datatabase_url but in requirements.txt dj-datatabase-url remember NOTE:
     
-# db_from_env= dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+db_from_env= dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
