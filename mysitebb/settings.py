061d51d28a3e5bc98dcb1a3b13358842a4fc57da
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2022-11-17 19:56:34 +0600

    Update settings.py

--- mysitebb/settings.py
+++ mysitebb/settings.py
@@ -176,7 +176,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            # "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [('containers-us-west-85.railway.app',7630)]
         },
     },
 }
