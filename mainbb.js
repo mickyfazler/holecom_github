4ef307bbe2b12341eacad6dedc64dcd97ff63fa4
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2024-05-06 17:37:26 +0600

    migrate and testing

--- mainbb.js
+++ mainbb.js
@@ -210,10 +210,10 @@ btnjoiny.addEventListener('click',(e)=>{
         console.log('endPoint',endPoint)
     */
     // webSockety = new WebSocket(endPoint);
-    webSockety = new WebSocket('wss://coral-app-g8iri.ondigitalocean.app/');
     // webSockety = new WebSocket('wss://holecomb.herokuapp.com/');
     // webSockety = new WebSocket('wss://web-production-807b.up.railway.app/');
-    // webSockety = new WebSocket('ws://127.0.0.1:8000/');
+    // webSockety = new WebSocket('wss://coral-app-g8iri.ondigitalocean.app/');
+    webSockety = new WebSocket('ws://127.0.0.1:8000/');
     webSockety.addEventListener('open',(e)=>{ // NOTE: he always write like this ....which is suck
     // webSockety.onopen = () =>{         // NOTE: learn from codewithsingh 
         console.log('connection openebb');
